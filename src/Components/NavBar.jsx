57a1cc365763ca9e4addabbe9f9a6b94dbef67e0
kuo-hm <hmouraoussama@gmail.com>
2021-08-18 00:44:15 +0100

    Navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,29 +1,65 @@
 import React from "react";
 import logo from "../Media/logo.png";
 import "./Nav.css";
-
+import { Link, animateScroll as scroll } from "react-scroll";
 const NavBar = () => {
   return (
-    <header className="head">
-      <img src={logo} className="logo" alt="" />
+    <div className="head">
+      <Link
+        activeClass="active"
+        to="who"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+      >
+        <img src={logo} className="logo" alt="" />
+      </Link>
       <input type="checkbox" id="nav-toggle" className="nav-toggle" />
       <nav>
         <ul>
           <li>
-            <a href="#/">Who are we?</a>
+            <Link
+              activeClass="active"
+              to="who"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              <a href="#/">Who are we?</a>
+            </Link>
           </li>
           <li>
-            <a href="#/">Our services</a>
+            <Link
+              activeClass="active"
+              to="services"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              <a href="#/">Our services</a>
+            </Link>
           </li>
           <li>
-            <a href="#/">Contact us</a>
+            <Link
+              activeClass="active"
+              to="contact-us"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              <a href="#/">Contact us</a>
+            </Link>
           </li>
         </ul>
       </nav>
       <label for="nav-toggle" className="nav-toggle-label">
         <span></span>
       </label>
-    </header>
+    </div>
   );
 };
 
